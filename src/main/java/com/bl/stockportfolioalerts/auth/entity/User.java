7ed5c45e6d381e699f5857c3669e063cf676efec
onizukaTP <tharunprabu2418@gmail.com>
2026-03-05 17:03:09 +0530

    feat(auth): add user entity for persistence [uc1]

---
+++ src/main/java/com/bl/stockportfolioalerts/auth/entity/User.java
@@ -0,0 +1,23 @@
+package com.bl.stockportfolioalerts.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
