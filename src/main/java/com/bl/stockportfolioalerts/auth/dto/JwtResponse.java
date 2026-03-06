8e1326273375adea3794e6cf2dbf74779c5272ba
onizukaTP <tharunprabu2418@gmail.com>
2026-03-06 14:40:35 +0530

    feat(auth): add jwt response dto for token return [uc2]

---
+++ src/main/java/com/bl/stockportfolioalerts/auth/dto/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.bl.stockportfolioalerts.auth.dto;
+
+import lombok.Getter;
+
+@Getter
+public class JwtResponse {
+
+    private String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+}
