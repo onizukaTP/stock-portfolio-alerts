8232e1fd0b6704e7283d325525c139919f12b956
onizukaTP <tharunprabu2418@gmail.com>
2026-03-06 14:41:49 +0530

    feat(security): implement custom user details service [uc2]

---
+++ src/main/java/com/bl/stockportfolioalerts/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.bl.stockportfolioalerts.auth.security;
+
+import com.bl.stockportfolioalerts.auth.entity.User;
+import com.bl.stockportfolioalerts.auth.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
