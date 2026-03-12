9fc6c7f9ce40833d711d11197709857989b2195a
onizukaTP <tharunprabu2418@gmail.com>
2026-03-12 16:34:33 +0530

    feat(portfolio): add optimistic locking using version field to prevent concurrent update conflicts [uc5]

--- src/main/java/com/bl/stockportfolioalerts/portfolio/entity/Portfolio.java
+++ src/main/java/com/bl/stockportfolioalerts/portfolio/entity/Portfolio.java
@@ -3,12 +3,18 @@ package com.bl.stockportfolioalerts.portfolio.entity;
 import com.bl.stockportfolioalerts.auth.entity.User;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Portfolio {
 
     @Id
@@ -16,6 +22,9 @@ public class Portfolio {
     private Long id;
 
     private double totalValue;
+
+    @Version
+    private Long version;
 
     @ManyToOne
     private User user;
