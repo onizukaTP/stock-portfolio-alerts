9c6ec6245a7b5a00dcf30610589b27530f6f276c
onizukaTP <tharunprabu2418@gmail.com>
2026-03-17 19:11:50 +0530

    feat(alert): add query method to fetch alerts by ticker [uc10]

--- src/main/java/com/bl/stockportfolioalerts/alert/repository/AlertRepository.java
+++ src/main/java/com/bl/stockportfolioalerts/alert/repository/AlertRepository.java
@@ -3,5 +3,8 @@ package com.bl.stockportfolioalerts.alert.repository;
 import com.bl.stockportfolioalerts.alert.entity.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlertRepository extends JpaRepository<Alert, Long> {
+    List<Alert> findByTicker(String ticker);
 }
