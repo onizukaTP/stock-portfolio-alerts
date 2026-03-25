237d7c4c04230ceeeced78533c0751a82b8f7019
onizukaTP <tharunprabu2418@gmail.com>
2026-03-25 14:11:13 +0530

    refactor(kafka): added kafka admin bean

--- src/main/java/com/bl/stockportfolioalerts/messaging/kafka/KafkaConfig.java
+++ src/main/java/com/bl/stockportfolioalerts/messaging/kafka/KafkaConfig.java
@@ -1,14 +1,31 @@
 package com.bl.stockportfolioalerts.messaging.kafka;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Map;
 
 @Configuration
 @EnableKafka
 public class KafkaConfig {
+
+    @PostConstruct
+    public void check() {
+        System.out.println("KafkaConfig LOADED");
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        return new KafkaAdmin(
+                Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+        );
+    }
 
     @Bean
     public NewTopic stockPriceTopic() {
