d5e04ace26ebe311ee6195f7f7865586358411ba
onizukaTP <tharunprabu2418@gmail.com>
2026-03-25 11:26:08 +0530

    refactor(messaging): reorganize RabbitMQ configuration for better structure and reuse

--- src/main/java/com/bl/stockportfolioalerts/messaging/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/bl/stockportfolioalerts/messaging/rabbitmq/RabbitMQConfig.java
@@ -1,5 +1,10 @@
 package com.bl.stockportfolioalerts.messaging.rabbitmq;
 
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.*;
 import org.springframework.amqp.core.Queue;
 
@@ -11,5 +16,32 @@ public class RabbitMQConfig {
     @Bean
     public Queue alertQueue() {
         return new Queue(ALERT_QUEUE, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter) {
+
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+
+        return factory;
     }
 }
