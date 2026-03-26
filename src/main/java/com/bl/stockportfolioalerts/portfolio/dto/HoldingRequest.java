89c296f9d505ce85f3cae413e61e5dedde11ad93
onizukaTP <tharunprabu2418@gmail.com>
2026-03-26 13:56:14 +0530

    refactor: added annotations

--- src/main/java/com/bl/stockportfolioalerts/portfolio/dto/HoldingRequest.java
+++ src/main/java/com/bl/stockportfolioalerts/portfolio/dto/HoldingRequest.java
@@ -1,8 +1,12 @@
 package com.bl.stockportfolioalerts.portfolio.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HoldingRequest {
 
     private String ticker;
